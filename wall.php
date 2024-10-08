7b3c7e5cedc47ccf358086624318e3b3fda980b4
Fiona <fiona.rio@outlook.com>
2024-10-08 09:55:45 +0200

    modification de news.php admin.php settings.php et début de wall.php

--- wall.php
+++ wall.php
@@ -40,7 +40,7 @@
             /**
              * Etape 2: se connecter à la base de donnée
              */
-            $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+            $mysqli = new mysqli("localhost", "root", "", "socialnetwork");
             ?>
 
             <aside>
@@ -57,7 +57,7 @@
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : XXX
+                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : <?php echo $user['alias'] ?>
                         (n° <?php echo $userId ?>)
                     </p>
                 </section>
