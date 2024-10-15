768afc766748a1d90d93af39de97e07ef56cddd8
Fiona <fiona.rio@outlook.com>
2024-10-15 12:15:38 +0200

    changements de titres

--- header.php
+++ header.php
@@ -3,14 +3,14 @@
     <nav id="menu">
         <a href="news.php">Actualités</a>
         <a href="wall.php">Mur</a>
-        <a href="feed.php">Flux</a>
-        <a href="tags.php">Mots-clés</a>
+        <a href="feed.php">Feed</a>
+        <a href="tags.php">Tendances</a>
     </nav>
     <nav id="user">
         <a href="#">Profil</a>
         <ul>
             <li><a href="settings.php">Paramètres</a></li>
-            <li><a href="followers.php">Mes suiveurs</a></li>
+            <li><a href="followers.php">Followers</a></li>
             <li><a href="subscriptions.php">Mes abonnements</a></li>
         </ul>
     </nav>
